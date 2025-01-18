2072a33c5ea6d9751b5250efecc211283d8df06d
eunbeann <ds5bean23@gmail.com>
2025-01-18 12:44:04 -0800

    feat: mobileSize

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,6 +12,9 @@ export default {
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
+      maxWidth: {
+        content: "420px",
+      },
     },
   },
   plugins: [],
