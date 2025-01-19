86969b419dfd73f71199bccdae6c6b63dd3325ce
David Lim <davidlim@dhcp-128-189-228-164.ubcsecure.wireless.ubc.ca>
2025-01-18 22:42:41 -0800

    setup home footer + added pages for events and scan

---
+++ src/app/app/events/page.tsx
@@ -0,0 +1,14 @@
+import FooterNav from "@/app/components/footerNav";
+
+FooterNav
+
+const EventsPage: React.FC = () => {
+    return (
+        <>
+            Events Page
+            <FooterNav />
+        </>
+    )
+}
+
+export default EventsPage;
