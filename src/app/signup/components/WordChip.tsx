fb0f2ccbd434b6c10d74e467205b0427e8c0bc76
eunbeann <ds5bean23@gmail.com>
2025-01-18 20:09:05 -0800

    feat: add selected props

--- src/app/signup/components/WordChip.tsx
+++ src/app/signup/components/WordChip.tsx
@@ -1,6 +1,16 @@
-export default function WordChip({ children }: { children: string }) {
+interface WordChipProps {
+  children: string;
+  selected: boolean;
+}
+
+export default function WordChip({ children, selected }: WordChipProps) {
   return (
-    <div className="rounded-[2rem] border-2 border-[#3F3632] py-1 px-4 text-lg text-[#3F3632] font-bold">
+    <div
+      className={`rounded-[2rem] border-2 py-1 px-5 text-lg font-bold border-[#3F3632] text-[#3F3632] ${
+        selected && "bg-[#fff] text-white"
+      }`}
+    >
+      {" "}
       {children}
     </div>
   );
