1ee5640bf84e0b0c2816ab3bf012a300d77988e3
eunbeann <ds5bean23@gmail.com>
2025-01-18 19:32:23 -0800

    feat: signup Page

---
+++ src/app/signup/page.tsx
@@ -0,0 +1,40 @@
+"use client";
+
+import { useSearchParams } from "next/navigation";
+import { useRouter } from "next/router";
+import { JSX, useEffect, useState } from "react";
+import GetName from "./steps/GetName";
+import PreferredGroupSize from "./steps/PreferredGroupSize";
+import WillingToCommute from "./steps/WillingToCommute";
+import YourInterest from "./steps/YourInterest";
+
+export default function SignupPage() {
+  const searchParams = useSearchParams();
+  const router = useRouter();
+  const [step, setStep] = useState<number>(0);
+
+  type StepComponents = {
+    [key: number]: JSX.Element;
+  };
+
+  useEffect(() => {
+    const stepParam = searchParams.get("step");
+
+    if (!stepParam) {
+      router.replace("?step=1");
+      setStep(1);
+    } else {
+      const parsedStep = parseInt(stepParam, 10);
+      setStep(parsedStep);
+    }
+  }, [searchParams, router]);
+
+  const stepComponents: StepComponents = {
+    0: <GetName />,
+    1: <YourInterest />,
+    2: <PreferredGroupSize />,
+    3: <WillingToCommute />,
+  };
+
+  return <>{stepComponents[step]}</>;
+}
