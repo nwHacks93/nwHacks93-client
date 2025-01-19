b2c33e35a3979c6781912ec84e73bb361e1dd6c1
eunbeann <ds5bean23@gmail.com>
2025-01-18 21:00:09 -0800

    feat: add fonts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,7 +1,11 @@
 import type { Config } from "tailwindcss";
 
 export default {
-  content: ["./src/pages/**/*.{js,ts,jsx,tsx,mdx}", "./src/components/**/*.{js,ts,jsx,tsx,mdx}", "./src/app/**/*.{js,ts,jsx,tsx,mdx}"],
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
   theme: {
     extend: {
       colors: {
@@ -14,15 +18,16 @@ export default {
         foreground: "var(--foreground)",
         "pastel-blue": "#A7C7E7",
         "pastel-purple": "#D6C3E0",
-        "pastel-green": "#B2E0B2"
+        "pastel-green": "#B2E0B2",
       },
       maxWidth: {
-        content: "420px"
+        content: "420px",
       },
       fontFamily: {
-        alegreya: ['Alegreya']
-      }
-    }
+        alegreya: ["Alegreya"],
+        alegreya_sans: ["Alegreya_sans"],
+      },
+    },
   },
-  plugins: []
+  plugins: [],
 } satisfies Config;
